c3d962c0ed2fb10b53036195a9c397fd198cc334
Mohamed Eid <m.mohamedeid11@gmail.com>
2022-10-12 16:28:54 +0200

    User registration, login and logout with Laravel Fortify Leasson 3

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+{{--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">--}}
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -17,7 +17,6 @@
     </head>
     <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        .container
         <div class="container">
             <a class="navbar-brand" href="#">{{config('app.name' , 'User Management System')}}</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -36,13 +35,39 @@
                     @if (Route::has('login'))
                         <div >
                             @auth
-                                <a href="{{ url('/home') }}" >Home</a>
-                            @else
-                                <a href="{{ route('login') }}" >Log in</a>
+                                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ url('/home') }}" >Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ url('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                                        <form id="logout-form" action="{{route('logout')}}" method="post" style="display: none">
+                                            @csrf
+                                        </form>
+                                    </li>
+                                </ul>
+{{--                                <a  href="{{ url('/home') }}" >Home</a>--}}
 
-                                @if (Route::has('register'))
-                                    <a href="{{ route('register') }}">Register</a>
-                                @endif
+{{--                                <a href="{{ url('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>--}}
+{{--                                <form id="logout-form" action="{{route('logout')}}" method="post" style="display: none">--}}
+{{--                                    @csrf--}}
+{{--                                </form>--}}
+                            @else
+                                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('login') }}" >Log in</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        @if (Route::has('register'))
+                                            <a class="nav-link" href="{{ route('register') }}">Register</a>
+                                        @endif
+                                    </li>
+                                </ul>
+{{--                                <a href="{{ route('login') }}" >Log in</a>--}}
+
+{{--                                @if (Route::has('register'))--}}
+{{--                                    <a class="nav-link" href="{{ route('register') }}">Register</a>--}}
+{{--                                @endif--}}
                             @endauth
                         </div>
                     @endif
